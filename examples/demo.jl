091850d207c2d8acdd1315fcfb3effeb29faaa2e
Raja Doake <rdoake@gmail.com>
2024-03-06 10:27:37 -0600

    Add CI, except docs (for now)

--- examples/demo.jl
+++ examples/demo.jl
@@ -1,4 +1,5 @@
 using ToyAD
+using Zygote
 
 x = DualNumber(3.0, 1.0)
 y = DualNumber(4.0, 1.0)
@@ -18,3 +19,30 @@ myfancyfunc(x) = (3x + 2) * (2x + 1)
 myfancyfuncprime(x) = 3 * (2x + 1) + 2 * (3x + 2)
 
 forward_diff(myfancyfunc, 5.0) == myfancyfuncprime(5.0)
+
+# exponentiation
+x^2
+
+powfunc(x) = x^3
+
+powfuncprime(x) = 3x^2
+
+forward_diff(expfunc, 5.) == powfuncprime(5.)
+
+powfancyfunc(x) = 4x^3 + 2x^2
+
+powfancyfuncprime(x) = 12x^2 + 4x
+
+forward_diff(powfancyfunc, 5.) == powfancyfuncprime(5.)
+
+powzerofunc(x) = 2 * x^0
+
+powzerofuncprime(x) = 0
+
+forward_diff(powzerofunc, 5.) == powzerofuncprime(5.)
+
+dualpowfunc(x) = x^DualNumber(2, 3)
+
+dualpowfunc(x)
+
+forward_diff(dualpowfunc, 5)
